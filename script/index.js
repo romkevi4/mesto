03d7a6d1a2775fa277730a4c3d211188fa9e610c
Roman Shatskikh <shatskikh.roman@gmail.com>
2022-01-27 16:56:05 +0300

    feat: сверстать кнопки delete карточек и добавить стили для них

--- script/index.js
+++ script/index.js
@@ -25,11 +25,16 @@ const initialCards = [
     link: 'https://images.unsplash.com/photo-1612899028149-ddc7969d27cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   }
 ];
+const cardImages = document.querySelectorAll('.element__image');
+const cardTitles = document.querySelectorAll('.element__title');
+
 let profileEditBtn = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__name');
 let profileAboutMe = document.querySelector('.profile__about-me');
 
 let placeAddBtn = document.querySelector('.profile__add-button');
+const placeDeleteBtn = document.querySelectorAll('.element__delete-button');
+const likeBtns = document.querySelectorAll('.element__like-button');
 
 let popupEdit = document.querySelector('#popup-edit');
 let formEditingProfile = document.forms.editingProfile;
@@ -42,21 +47,28 @@ let popupPlaceTitle = formAddPlace.elements.popupTitle;
 let popupPlaceLink = formAddPlace.elements.popupLink;
 
 const popupCloseBtns = document.querySelectorAll('.popup__close-button');
-// const popupSaveBtns = document.querySelectorAll('.popup__save-button');
+const popupSaveBtns = document.querySelectorAll('.popup__save-button');
 
-const likeBtns = document.querySelectorAll('.element__like-button');
+
+// Adding cards by default
+// initialCards.forEach( (card) => {
+//   cardTitles.forEach( (title) => {
+//     title.textContent = card.name;
+//     console.log(title.textContent);
+//   });
+// });
 
 
 // Events
 profileEditBtn.addEventListener('click', popupEditOpen);
-placeAddBtn.addEventListener('click', popupEditOpen);
+formEditingProfile.addEventListener('submit', saveForm);
+
+placeAddBtn.addEventListener('click', popupAddOpen);
+formAddPlace.addEventListener('submit', saveForm);
+
 popupCloseBtns.forEach((element) => {
   element.addEventListener('click', popupClose);
 });
-formEditingProfile.addEventListener('submit', saveForm);
-
-formAddPlace.addEventListener('submit', saveForm);
-
 
 likeBtns.forEach((element) => {
   element.addEventListener('click', (elem) => {
@@ -67,19 +79,26 @@ likeBtns.forEach((element) => {
 
 // Function
 function popupEditOpen() {
-  popupEdit.target.classList.add('popup_opened');
+  popupEdit.classList.add('popup_opened');
   popupProfileName.value = profileName.textContent;
   popupProfileAboutMe.value = profileAboutMe.textContent;
 }
 
+function popupAddOpen() {
+  popupAdd.classList.add('popup_opened');
+}
 
-function popupClose(evt) {
-  evt.target.classList.remove('popup_opened');
+function popupClose(elem) {
+  elem.target.closest('.popup').classList.remove('popup_opened');
 }
 
 function saveForm(evt) {
   evt.preventDefault();
-  profileName.textContent = popupProfileName.value;
-  profileAboutMe.textContent = popupProfileAboutMe.value;
-  popupClose();
+  if (evt.target.closest('#popup-edit')) {
+    profileName.textContent = popupProfileName.value;
+    profileAboutMe.textContent = popupProfileAboutMe.value;
+    popupEdit.classList.remove('popup_opened');
+  } else if (evt.target.closest('#popup-add')) {
+    popupAdd.classList.remove('popup_opened');
+  }
 }
